7cb775ec5186931ccc7dfab0b025edaa370a1121
MezaCode <60710916+MezaCode@users.noreply.github.com>
2021-10-09 04:20:51 -0500

    created Player.java & updated WarWithouRecycling.java
    created Player.java for the implementation of cohesion.
    Player.java will now keep track of each players cards and points.
    
    WarWithoutRecycling.java now about complete and ready for polymorphism, also it's now only in charge of game rules from managing the rounds & checking for a winner, to awarding points and ending the game.

--- WarWithoutRecycling.java
+++ WarWithoutRecycling.java
@@ -1,101 +1,123 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
-import java.util.LinkedList;
 
 public class WarWithoutRecycling {
     ArrayList<Card> masterDeck;
-    LinkedList<Card> player1Deck;
-    LinkedList<Card> player2Deck;
-    ArrayList<Card> player1PointPile;
-    ArrayList<Card> player2PointPile;
-    ArrayList<Card> player1FaceUpCards;
-    ArrayList<Card> player2FaceUpCards;
-    ArrayList<Card> player1FaceDownCards;
-    ArrayList<Card> player2FaceDownCards;
+    Player player1;
+    Player player2;
+    int warRound;
 
     public WarWithoutRecycling() {
+        warRound = -1; // -1 to indicate it's outside of war round
         initializeDecks();
     }
 
     public void initializeDecks(){
         this.masterDeck = new Deck().getDeckOfCards();
         Collections.shuffle(this.masterDeck);
-        this.player1Deck = new LinkedList<Card>();
-        this.player2Deck = new LinkedList<Card>();
-        this.player1Deck.addAll(masterDeck.subList(0,25));
-        this.player2Deck.addAll(masterDeck.subList(26,masterDeck.size()));
-        this.player1PointPile = new ArrayList<Card>();
-        this.player2PointPile = new ArrayList<Card>();
-        this.player1FaceDownCards = new ArrayList<Card>();
-        this.player1FaceUpCards = new ArrayList<Card>();
-        this.player2FaceDownCards = new ArrayList<Card>();
-        this.player2FaceUpCards = new ArrayList<Card>();
+        this.player1 = new Player("Player 1",masterDeck.subList(0,25));
+        this.player2 = new Player("Player 2",masterDeck.subList(26,masterDeck.size()));
     }
 
-    public Card getCard(int player){
-        Card playerCard;
-        if (player == 1){
-            playerCard = player1Deck.pop();
+    public void playRound(){
+        if (player1.getHandSize() == 0 || player2.getHandSize() == 0){
+            endGame();
         }
-        else{
-            playerCard = player2Deck.pop();
+        Card player1Card = player1.getRoundCard();
+        Card player2Card = player2.getRoundCard();
+
+        if (player1Card.getRank() > player2Card.getRank()){
+            roundEnd( player1, player2, player1Card, player2Card);
         }
-        return playerCard;
+        else if (player1Card.getRank() < player2Card.getRank()){
+            roundEnd( player2, player1, player1Card, player2Card);
+        }
+        else {
+            System.out.println("*** WAR! ***");
+            war(player1Card, player2Card);
+        }
     }
-
-    public void roundWinner( int player, ArrayList<Card> roundsCards){
-        if (player == 1){
-            player1PointPile.addAll(roundsCards);
-            System.out.println("PLayer 1 wins the round");
-        }
-        else{
-            player2PointPile.addAll(roundsCards);
-            System.out.println("PLayer 2 wins the round");
-        }
-
-        System.out.println("Player 1 has a score of " + player1PointPile.size());
-        System.out.println("Player 2 has a score of " + player2PointPile.size());
+// todo: roundEnd method needs to be changed for polymorphism
+    //change addToPointPile method into addToHand method
+    public void roundEnd( Player winner, Player loser, Card player1Card, Card player2Card){
+        winner.addToPointPile(getWinnerCards(player1Card, player2Card));
+        winner.wins(winner.getPointPileSize());
+        loser.loses(loser.getPointPileSize());
     }
 
     public void war(Card player1Card, Card player2Card){
-        boolean winnerNotFound = true;
-        int warRound = 0;
+        boolean winnerNotFound = checkWarAble(player1Card, player2Card);
+        warRound = 0;
         ArrayList<Card> warRoundCards = new ArrayList<Card>();
         while (winnerNotFound){
             getWarCards();
-
-            if (player1FaceUpCards.get(warRound).getRank() > player2FaceUpCards.get(warRound).getRank()){
+            if (player1.getFaceUpCards().get(warRound).getRank() > player2.getFaceUpCards().get(warRound).getRank()){
                 winnerNotFound = false;
-                warRoundCards.addAll(getWarWinnerCards(player1Card, player2Card));
-                roundWinner(1, warRoundCards);
+                roundEnd( player1, player2, player1Card, player2Card);
             }
-            else if (player1FaceUpCards.get(warRound).getRank() < player2FaceUpCards.get(warRound).getRank()){
+            else if (player1.getFaceUpCards().get(warRound).getRank() < player2.getFaceUpCards().get(warRound).getRank()){
                 winnerNotFound = false;
-                warRoundCards.addAll(getWarWinnerCards(player1Card, player2Card));
-                roundWinner(2, warRoundCards);
+                roundEnd( player2, player1, player1Card, player2Card);
             }
-            else if (player1Deck.size() <=1 || player2Deck.size() <=1){
-                //Todo: figure out which unofficial rule to use for 1 or less cards remaining in a player's deck
+            else {
+                winnerNotFound = checkWarAble(player1Card, player2Card);
+                warRound++;
             }
-            else { warRound++;}
         }
     }
-    public void getWarCards(){
-        player1FaceDownCards.add(player1Deck.pop());
-        player1FaceUpCards.add(player1Deck.pop());
-        player2FaceDownCards.add(player2Deck.pop());
-        player2FaceUpCards.add(player2Deck.pop());
+
+    public boolean checkWarAble(Card player1Card, Card player2Card){
+        if (player1.getHandSize() <=1  && player2.getHandSize() <=1 ){
+            player1.warTie(player1Card);
+            player2.warTie(player2Card);
+            System.out.println("Both Players unable to continue War, Cards have been returned to bottom of deck");
+            return false;
+        }
+        else if (player1.getHandSize() <=1){
+            System.out.println("Player 1 unable to continue War");
+            roundEnd( player2, player1, player1Card, player2Card);
+            return false;
+        }
+        else if (player2.getHandSize() <=1){
+            System.out.println("Player 2 unable to continue War");
+            roundEnd( player1, player2, player1Card, player2Card);
+            return false;
+        }
+        else { return true;}
     }
-    public ArrayList<Card> getWarWinnerCards(Card player1Card, Card player2Card){
-        ArrayList<Card> warWinnings = new ArrayList<Card>();
-        warWinnings.addAll(player1FaceUpCards);
-        warWinnings.addAll(player1FaceDownCards);
-        warWinnings.addAll(player2FaceUpCards);
-        warWinnings.addAll(player2FaceDownCards);
-        warWinnings.add(player1Card);
-        warWinnings.add(player2Card);
-        return warWinnings;
+
+    public void getWarCards(){
+        player1.drawWarCards();
+        player2.drawWarCards();
+    }
+    public ArrayList<Card> getWinnerCards(Card player1Card, Card player2Card){
+        ArrayList<Card> winnings = new ArrayList<Card>();
+        if (warRound >= 0) {
+            winnings.addAll(player1.getFaceUpCards());
+            winnings.addAll(player1.getFaceDownCards());
+            winnings.addAll(player2.getFaceUpCards());
+            winnings.addAll(player2.getFaceDownCards());
+            warRound = -1;
+        }
+        winnings.add(player1Card);
+        winnings.add(player2Card);
+        return winnings;
+    }
+    // todo: change endGame method for polymorphism
+    // change getPointPileSize method to getHandSize method
+    public void endGame(){
+        System.out.println("*** GAME OVER ***");
+        if (player1.getPointPileSize() > player2.getPointPileSize()){
+            System.out.println(player1.getName() + " WINS THE GAME!");
+        }
+        else if (player1.getPointPileSize() < player2.getPointPileSize()){
+            System.out.println(player2.getName() + " WINS THE GAME!");
+        }
+        else{ System.out.println("*** TIE GAME ***");}
+
+        System.out.println(player1.getName() +" has a score of " + player1.getPointPileSize());
+        System.out.println(player1.getName() +" has a score of " + player1.getPointPileSize());
+        System.exit(0);
     }
 
 }
